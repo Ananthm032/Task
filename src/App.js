ac04be5643fbad456d84b3ca150e5428171b554e
Ananth <106952901+Ananthm032@users.noreply.github.com>
2023-10-21 17:06:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "./App.css"
 
-function App() {
+const QuoteWelcome = () => {
+  const [quote, setQuote] = useState('');
+  const [notificationShown, setNotificationShown] = useState(false);
+
+  // Fetch the quote api from backend
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        const response = await fetch('https://quotes-virid.vercel.app/api');
+        if (response.ok) {
+          const data = await response.json();
+          const randomQuote = data[Math.floor(Math.random() * data.length)].quote;
+          setQuote(randomQuote);
+
+          // Show notification only if it hasn't been shown yet
+          if (!notificationShown) {
+            showNotification("Welcome to React");
+            setNotificationShown(true); // Set notificationShown to true once the notification is shown
+          }
+        } else {
+          console.error('Error fetching quote:', response.status);
+        }
+      } catch (error) {
+        console.error('Error fetching quote:', error);
+      }
+    };
+
+    // Fetch quote when the component mounts
+    fetchQuote();
+
+    // Set up interval to fetch a new quote every 2 minutes
+    const interval = setInterval(fetchQuote, 120000); // 2 minutes in milliseconds
+
+    // Clean up the interval to avoid memory leaks
+    return () => clearInterval(interval);
+  }, [notificationShown]); // Add notificationShown as a dependency to useEffect
+
+  // Toastify notification
+  const showNotification = message => {
+    toast('🦄 Welcome to React', {
+      position: "bottom-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quote-welcome">
+      <div className='quote-container'>
+        <h1>"Welcome Quote of the Day."</h1>
+        <p>{quote}</p>
+      </div>
+      <ToastContainer />
     </div>
   );
-}
+};
 
-export default App;
+export default QuoteWelcome;
